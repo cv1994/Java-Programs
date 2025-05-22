63cde684a4db63e09a5c24ab7acceed9b2371909
cv1994 <145704083+cv1994@users.noreply.github.com>
2025-05-22 23:46:23 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,11 @@
+public class Average {
+public static void main(String[] args) {
+int[] numbers = {1,2,3,4,5};
+double sum= 0;
+for (int i=0; i<numbers.length; i++) {
+sum += numbers[i];
+}
+double average=sum/numbers.length;
+System.out.println("The average is:" +average);
+ }
+}
